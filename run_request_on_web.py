c53a2a87204deea3aca42b6193e6d46fc4017790
huythanh0x <huythanh0x@gmail.com>
2021-11-17 14:46:44 +0700

    update head at test request in server

--- run_request_on_web.py
+++ run_request_on_web.py
@@ -25,8 +25,8 @@ def discudemy():
     du_links = []
     big_all = []
     head = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.128 Safari/537.36 Edg/89.0.774.77",
-        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Language" : "en-US,en;q=0.5",
+    "User-Agent": "Defined",
     }
 
     for page in range(1, PAGE_NTH):
